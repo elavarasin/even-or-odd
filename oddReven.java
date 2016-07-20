120f65163d98eddb00e1c2105befe1718daa3e36
elavarasin <elavarasi1596@gmail.com>
2016-07-20 11:42:25 +0530

    odd
    ass

---
+++ oddReven.java
@@ -0,0 +1,15 @@
+public class ass {
+
+		 public static void main(String[] args) {
+			 int x=Integer.parseInt(args[0]);
+			 System.out.println("enter the number:"+x);
+			 if(x%2==0){
+			 System.out.println("even number");
+			 }
+			 else
+			 {
+			 
+			 System.out.println("odd number");
+			 	}
+			 }
+}
